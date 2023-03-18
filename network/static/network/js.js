cbf492882f71e6c977b1ccb71bd603c4b4ac7228
Mohsen Pasdar <mohsenpasdar@gmail.com>
2023-03-18 13:33:50 +0330

    -continued: Specifications: Profile Page completed

--- network/static/network/js.js
+++ network/static/network/js.js
@@ -1,7 +1,4 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const followForm = document.getElementById("follow-form");
-    const unfollowForm = document.getElementById("unfollow-form");
-
     // Select all SVG elements with an ID that starts with "heart-icon-"
     const svgs = document.querySelectorAll("[id^='heart-icon-']");
     // Loop through each SVG element
@@ -13,67 +10,39 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
 
-    if (followForm) {
-        const viewedUserUsername = followForm.getAttribute('data-viewed-user-username');
+    const followForm = document.getElementById("follow-form");
+    const viewedUserUsername = followForm.getAttribute('data-viewed-user-username');
+    const followBtn = document.getElementById("follow-btn");
 
-        followForm.addEventListener("submit", (event) => {
-            event.preventDefault();
-            console.log(`/follow/${viewedUserUsername}/`);
-            fetch(`/follow/${viewedUserUsername}/`, {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-            })
-            .then((response) => {
-                if (response.status === 201) {
-                    return response.json();
-                }
-                else {
-                    throw new Error('Cannot unfollow yourself.');
-                }
-            })
-            .then((data) => {
-                // update UI, e.g. change button text, show success message
-                console.log(data);                
-                const numFollowers = document.querySelector(".profile-info > p:first-of-type");
-                numFollowers.innerText = `Followers: ${data.num_followers}`;
-            })
-            .catch((error) => {
-                // show error message
-            });
+    followForm.addEventListener("submit", (event) => {
+        event.preventDefault();
+        const url = followBtn.innerText === 'Follow' ? `/follow/${viewedUserUsername}/` : `/unfollow/${viewedUserUsername}/`;
+        fetch(url, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+        })
+        .then((response) => {
+            if (response.status === 201) {
+                return response.json();
+            }
+            else {
+                throw new Error('Cannot unfollow yourself.');
+            }
+        })
+        .then((data) => {
+            // update UI, e.g. change button text, show success message
+            if (followBtn.innerText === "Follow") {
+                followBtn.innerText = "Unfollow";
+            } else {
+                followBtn.innerText = "Follow";
+            }
+            const numFollowers = document.querySelector(".profile-info > p:first-of-type");
+            numFollowers.innerText = `Followers: ${data.num_followers}`;
+        })
+        .catch((error) => {
+            console.log(error);
         });
-    }
-
-    if (unfollowForm) {
-        const viewedUserUsername = unfollowForm.getAttribute('data-viewed-user-username');
-
-        unfollowForm.addEventListener("submit", (event) => {
-            event.preventDefault();
-            console.log(`/unfollow/${viewedUserUsername}/`);
-            fetch(`/unfollow/${viewedUserUsername}/`, {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-            })
-            .then((response) => {
-                if (response.status === 201) {
-                    return response.json();
-                }
-                else {
-                    throw new Error('Cannot unfollow yourself.');
-                }
-            })
-            .then((data) => {
-                // update UI, e.g. change button text, show success message
-                console.log(data);
-                const numFollowers = document.querySelector(".profile-info > p:first-of-type");
-                numFollowers.innerText = `Followers: ${data.num_followers}`;
-            })
-            .catch((error) => {
-                // show error message
-            });
-        });
-    }
+    });
 })
